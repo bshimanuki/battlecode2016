c9814c6d2b784e0e6b55cdbf3edd84ddc12fe3f9
Brian Shimanuki <bshimanuki@mit.edu>
2016-01-25 02:44:38 -0500

    stricter turret

--- src/team391/Archon.java
+++ src/team391/Archon.java
@@ -128,7 +128,7 @@ class Archon extends Model {
             RobotType typeToBuild = RobotType.SCOUT;
             if(rc.getRoundNum() > 40 && canBuildViper && !Common.hasViper)
                 typeToBuild = RobotType.VIPER;
-            else if(rc.getTeamParts() > 500 && Common.buildSpaces(rc, RobotType.TURRET) > 4){
+            else if(rc.getTeamParts() > 500 && Common.allies.length > 8 && Common.archonIdsSize <= 2 && Common.buildSpaces(rc, RobotType.TURRET) > 4){
                 typeToBuild = RobotType.TURRET;
             }
             // Check for sufficient parts
