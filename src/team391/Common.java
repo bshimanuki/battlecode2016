cbdc1c11334bfbe0477248942c65f12f61e387b3
Brian Shimanuki <bshimanuki@mit.edu>
2016-01-25 20:59:50 -0500

    further signal archon

--- src/team391/Common.java
+++ src/team391/Common.java
@@ -240,8 +240,8 @@ class Common {
         closestZombies = closestRobots(zombies);
         closestAllies = closestRobots(allies);
         for(RobotInfo info : allRobots) {
-            if(info.type == RobotType.ARCHON) {
-                if(knownLocations[info.ID%ID_MOD] == null || knownLocations[info.ID%ID_MOD].distanceSquaredTo(info.location) > 100)
+            if(robotType == RobotType.SCOUT && info.type == RobotType.ARCHON) {
+                if(knownLocations[info.ID%ID_MOD] == null || knownLocations[info.ID%ID_MOD].distanceSquaredTo(info.location) > 200)
                     new SignalUnit(info).addFull();
             }
             addInfo(info);
